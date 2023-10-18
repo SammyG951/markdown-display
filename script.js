a06a24117b7cdafe01fe80c61652e107bafe4b4c
Sammy Gonzalez III <sammygonzo951@gmail.com>
2023-10-17 22:05:02 -0700

    Implemented styles

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ linkSubmitButton.addEventListener('click', () => {
             const converter = new showdown.Converter();
             const html = converter.makeHtml(data);
             contentElement.innerHTML = html;
+            contentElement.className = "markdown-content-section";
 
             linkTextBox.value = '';
         })
@@ -48,6 +49,7 @@ uploadButton.addEventListener('click', () => {
                 const converter = new showdown.Converter();
                 const html = converter.makeHtml(fileContent);
                 contentElement.innerHTML = html;
+                contentElement.className = "markdown-content-section";
 
                 uploadInput.value = '';
             };
